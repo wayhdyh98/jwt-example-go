0e213da719a8852fa1e64f515b2d2d59e020a469
Wahyu Hidayah <spongebob.krabby@gmail.com>
2023-04-09 09:53:39 +0700

    update in product models

--- models/product.go
+++ models/product.go
@@ -7,8 +7,8 @@ import (
 
 type Product struct {
 	GormModel
-	Title       string `json:"title" form:"title" valid:"required-Title of your product is required"`
-	Description string `json:"description" form:"description" valid:"required-Description of your product is required"`
+	Title       string `json:"title" form:"title" valid:"required~Title of your product is required"`
+	Description string `json:"description" form:"description" valid:"required~Description of your product is required"`
 	UserID      uint
 	User        *User
 }
